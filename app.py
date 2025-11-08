8e8fcda089ea4eb6f2325feb0301cdfd57c31ff6
user45-desgin <uar0405@gmail.com>
2025-11-08 18:03:09 +0530

    redeployment

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from your CI/CD pipeline!"
+    return "updated pipeline"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
+#########for redeployment...!##################################
 
